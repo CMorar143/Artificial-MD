c9c37c9e89036ac1ed0aad2f331650f596a54cd5
CMorar143 <cian@morar.org>
2019-11-25 23:30:30 +0000

    training K nearest neighbour

--- Classifier.py
+++ Classifier.py
@@ -34,15 +34,18 @@ def train_model():
 	plt.show()
 	plt.close()
 
+
 	# Show a histogram of all the columns
 	heart.hist()
 	plt.show()
 	plt.close()
 	
+
 	# Show the amount of entries who have and don't have heart disease
 	plt.bar(['Does have heart disease', 'Does not have heart disease'], heart['target'].value_counts(), color = ['red', 'blue'])
 	plt.ylabel('Count')
 	plt.show()
+
 
 	# 
 	heart = pd.get_dummies(heart, columns = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'ca', 'thal'])
@@ -50,9 +53,28 @@ def train_model():
 	columns_to_scale = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
 	heart[columns_to_scale] = standardScaler.fit_transform(heart[columns_to_scale])
 
+
 	y = heart['target']
 	X = heart.drop(['target'], axis = 1)
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 0)
+
+
+	knn_scores = []
+	for k in range(1,21):
+		knn_classifier = KNeighborsClassifier(n_neighbors = k)
+		knn_classifier.fit(X_train, y_train)
+		knn_scores.append(knn_classifier.score(X_test, y_test))
+
+
+	plt.plot([k for k in range(1, 21)], knn_scores, color = 'red')
+	for i in range(1,21):
+		plt.text(i, knn_scores[i-1], (i, knn_scores[i-1]))
+	plt.xticks([i for i in range(1, 21)])
+	plt.xlabel('Number of Neighbors (K)')
+	plt.ylabel('Scores')
+	plt.title('K Neighbors Classifier scores for different K values')
+	plt.show()
+
 
 
 train_model()
