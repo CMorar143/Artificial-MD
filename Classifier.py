a13cdbe1c4b04c07d84d7ea244ca07a664014ee2
CMorar143 <cian@morar.org>
2019-11-25 17:54:00 +0000

    imported libraries

---
+++ Classifier.py
@@ -0,0 +1,7 @@
+# Start by importing libraries
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScalar
+from sklearn.neighbors import KNeighborsClassifier
