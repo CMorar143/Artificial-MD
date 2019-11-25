74b13ff7888d0b50daa9cf5bbaf30ca4a79624be
CMorar143 <cian@morar.org>
2019-11-25 17:57:59 +0000

    fixed import and imported data to DF

--- Classifier.py
+++ Classifier.py
@@ -3,5 +3,14 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScalar
+from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
+
+def train_model():
+	# Load heart disease dataset into pandas dataframe
+	pathHeart = "../Data/heart-disease-uci/"
+	heart = pd.read_csv(pathHeart + 'heart.csv')
+
+	print(heart.describe())
+
+train_model()
