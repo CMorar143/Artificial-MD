0a911207ad1d5e588c62cbfd0a8ac3d1fbb183ae
CMorar143 <cian@morar.org>
2019-11-25 23:54:11 +0000

    added Django project

---
+++ DjangoProj/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\cianmo~1\docume~1\colleg~1\year4\semest~1\fyp\coding\django~1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
