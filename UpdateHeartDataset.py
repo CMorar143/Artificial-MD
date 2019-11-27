8d8cbc7b7a1c2d3cff742413fdc070b61148988a
CMorar143 <cian@morar.org>
2019-11-27 14:40:04 +0000

    replaced all spaces with commas

--- UpdateHeartDataset.py
+++ UpdateHeartDataset.py
@@ -9,11 +9,15 @@ def process():
 	heart = pd.read_csv(pathHeart + 'heart.csv')
 
 	# Read in all the original datasets
-	cleveland = pd.read_csv(pathHeart + 'cleveland.txt', header=None)
-	hungarian = pd.read_csv(pathHeart + 'hungarian.txt', header=None)
-	switzerland = pd.read_csv(pathHeart + 'switzerland.txt', header=None)
+	cleveland = pd.read_csv(pathHeart + 'cleveland.txt', header=None).replace(' ', ',', regex=True)
+	hungarian = pd.read_csv(pathHeart + 'hungarian.txt', header=None).replace(' ', ',', regex=True)
+	switzerland = pd.read_csv(pathHeart + 'switzerland.txt', header=None).replace(' ', ',', regex=True)
 
-	
+	# Replace all spaces with a comma
+	# print(cleveland.replace(' ', ',', regex=True))
+
+	print(cleveland)
+
 	# for _, row in cleveland.iterrows():
 	# 	print(row[0])
 
