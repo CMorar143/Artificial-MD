97537c144bfc03cf32ac54b6e826a3a572c94c1e
CMorar143 <cian@morar.org>
2019-11-27 16:29:17 +0000

    specifying which columns will be used

--- UpdateHeartDataset.py
+++ UpdateHeartDataset.py
@@ -2,16 +2,55 @@ import os
 import csv
 import pandas as pd
 
+path_heart = "../Data/heart-disease-uci/"
+
+# 3 age: age in years
+# 4 sex: sex (1 = male; 0 = female)
+# 9 cp: chest pain type
+# 	-- Value 1: typical angina
+# 	-- Value 2: atypical angina
+# 	-- Value 3: non-anginal pain
+# 	-- Value 4: asymptomatic
+# 10 trestbps: resting blood pressure (in mm Hg on admission to the hospital)
+# 12 chol: serum cholestoral in mg/dl
+# 14 cigs (cigarettes per day)
+# 15 years (number of years as a smoker)
+# 16 fbs: (fasting blood sugar > 120 mg/dl) (1 = true; 0 = false)
+# 17 dm (1 = history of diabetes; 0 = no such history)
+# 18 famhist: family history of coronary artery disease (1 = yes; 0 = no)
+# 33 thalrest: resting heart rate
+
+# 37 trestbpd: resting blood pressure
+# 38 exang: exercise induced angina (1 = yes; 0 = no)
+
+# Input parameters
+heart_columns = {
+	3: 'age',
+	4: 'sex',
+	9: 'cp',
+	10: 'trestbpd',
+	12: 'chol',
+	14: 'cigs',
+	15: 'years',
+	16: 'fbs',
+	17: 'dm',
+	18: 'famhist',
+	33: 'thalrest',
+	37: 'trestbpd',
+	38: 'exang',
+	58: 'target'
+}
+
 def process_csv():
-	pathHeart = "../Data/heart-disease-uci/"
+	# pathHeart = "../Data/heart-disease-uci/"
 
 	# Read in dataset
-	heart = pd.read_csv(pathHeart + 'heart.csv')
+	heart = pd.read_csv(path_heart + 'heart.csv')
 
 	# Read in all the original datasets
-	cleveland = pd.read_csv(pathHeart + 'cleveland.txt', header=None).replace(' ', ',', regex=True)
-	hungarian = pd.read_csv(pathHeart + 'hungarian.txt', header=None).replace(' ', ',', regex=True)
-	switzerland = pd.read_csv(pathHeart + 'switzerland.txt', header=None).replace(' ', ',', regex=True)
+	cleveland = pd.read_csv(path_heart + 'cleveland.txt', header=None).replace(' ', ',', regex=True)
+	hungarian = pd.read_csv(path_heart + 'hungarian.txt', header=None).replace(' ', ',', regex=True)
+	switzerland = pd.read_csv(path_heart + 'switzerland.txt', header=None).replace(' ', ',', regex=True)
 
 	cleveland = cleveland.replace(r'\s', ' ', regex=True)
 	print(cleveland)
@@ -26,8 +65,7 @@ def process_csv():
 
 
 def text_file():
-	pathHeart = "../Data/heart-disease-uci/"
-	cleveland = open(pathHeart + 'cleveland.txt', mode='r')
+	cleveland = open(path_heart + 'cleveland.txt', mode='r')
 
 	# Read all the lines in the text file
 	all_lines = cleveland.readlines()
@@ -40,7 +78,7 @@ def text_file():
 
 	
 	# Create a new file
-	new_cleveland = open(pathHeart + 'new_cleveland.txt', 'w')
+	new_cleveland = open(path_heart + 'new_cleveland.txt', 'w')
 
 	for line in all_lines:
 		for word in line:
