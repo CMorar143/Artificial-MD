aa65bfc991446d1026577575e85f5a68254043ad
CMorar143 <cian@morar.org>
2019-12-01 14:33:23 +0000

    cleaning code

--- UpdateHeartDataset.py
+++ UpdateHeartDataset.py
@@ -56,7 +56,7 @@ def flatten(lst):
         else:
             yield elem
 
-# Write the data to a new file
+# Write the data to a new txt file
 def write_new_file(final_list):
 	# Create a new file
 	new_cleveland = open(path_heart + 'new_cleveland.txt', 'w')
@@ -70,6 +70,7 @@ def write_new_file(final_list):
 	new_cleveland.close()
 
 
+# Write the data to a new csv file
 def write_csv_file(df):
 	df.to_csv(path_heart + 'new_cleveland.csv', encoding='utf-8', index=False)
 
@@ -139,87 +140,7 @@ def create_dataset():
 	return extracted_params
 
 
-def train_model():
-	extracted_params = create_dataset()
-	
-	heart = pd.DataFrame(extracted_params, columns = list(heart_columns.values()))
-	# write_csv_file(heart)
-	# print(heart.head())
-
-	# Show correlation between features
-	plt.matshow(heart.corr())
-	plt.xticks(np.arange(heart.shape[1]), heart.columns)
-	plt.yticks(np.arange(heart.shape[1]), heart.columns)
-	plt.colorbar()
-	plt.show()
-	plt.close()
-
-	# Show a histogram of all the columns
-	heart.hist()
-	plt.show()
-	plt.close()	
-
-	# Show the amount of entries who have and don't have heart disease
-	plt.bar(['Does not have heart disease', 'Does have heart disease'], heart['target'].value_counts(), color = ['blue', 'red'])
-	plt.ylabel('Count')
-	plt.show()
-
-	# Use dummy columns for the categorical features
-	heart = pd.get_dummies(heart, columns = ['sex', 'cp', 'fbs', 'dm', 'famhist', 'exang'])
-	columns_to_scale = ['age', 'trestbps', 'chol', 'cigs', 'years', 'thalrest', 'trestbpd']
-	standardScaler = StandardScaler()
-	heart[columns_to_scale] = standardScaler.fit_transform(heart[columns_to_scale])
-
-	y = heart['target']
-	X = heart.drop(['target'], axis = 1)
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 0)
-
-	# KNN
-	knn_scores = []
-	for k in range(1,21):
-		knn_classifier = KNeighborsClassifier(n_neighbors = k)
-		knn_classifier.fit(X_train, y_train)
-		knn_scores.append(knn_classifier.score(X_test, y_test))
-
-	plt.plot([k for k in range(1, 21)], knn_scores, color = 'red')
-	for i in range(1,21):
-		plt.text(i, knn_scores[i-1], (i, knn_scores[i-1]))
-	plt.xticks([i for i in range(1, 21)])
-	plt.xlabel('Number of Neighbors (K)')
-	plt.ylabel('Scores')
-	plt.title('K Neighbors Classifier scores for different K values')
-	plt.show()
-
-
-	# Decision Tree
-	dt_scores = []
-	for i in range(1, len(X.columns) + 1):
-		dt_classifier = DecisionTreeClassifier(max_features = i, random_state = 0)
-		dt_classifier.fit(X_train, y_train)
-		dt_scores.append(dt_classifier.score(X_test, y_test))
-
-	plt.plot([i for i in range(1, len(X.columns) + 1)], dt_scores, color = 'green')
-	for i in range(1, len(X.columns) + 1):
-		plt.text(i, dt_scores[i-1], (i, dt_scores[i-1]))
-	plt.xticks([i for i in range(1, len(X.columns) + 1)])
-	plt.xlabel('Max features')
-	plt.ylabel('Scores')
-	plt.title('Decision Tree Classifier scores for different number of maximum features')
-	plt.show()
-
-
-
-
-def is_num(string):
-	try:
-		float(string)
-		return True
-	except ValueError:
-		return False
-
-# train_model()
 create_dataset()
-# process_csv()
 
 # Not being used
 def process_csv():
