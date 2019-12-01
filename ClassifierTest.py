861fec5ea0f73d3c9ca6f43d21a4e2247668137d
CMorar143 <cian@morar.org>
2019-12-01 14:24:51 +0000

    added to csv file

--- ClassifierTest.py
+++ ClassifierTest.py
@@ -10,9 +10,22 @@ from sklearn.tree import DecisionTreeClassifier
 def train_model():
 	# Load heart disease dataset into pandas dataframe
 	pathHeart = "../Data/heart-disease-uci/"
-	heart = pd.read_csv(pathHeart + 'new_cleveland.csv')
+	heart = pd.read_csv(pathHeart + 'heart.csv')
 
 	print(heart.head())
+
+	# Remove columns that are not going to be used
+	heart_prediction_columns = [
+		'age',
+		'sex',
+		'cp',
+		'trestbps',
+		'chol',
+		'fbs',
+		'target'
+	]
+
+	heart = heart[heart_prediction_columns]
 	
 	# Show correlation between features
 	plt.matshow(heart.corr())
@@ -22,21 +35,27 @@ def train_model():
 	plt.show()
 	plt.close()
 
+
 	# Show a histogram of all the columns
 	heart.hist()
 	plt.show()
-	plt.close()	
+	plt.close()
+	
 
 	# Show the amount of entries who have and don't have heart disease
-	plt.bar(['Does not have heart disease', 'Does have heart disease'], heart['target'].value_counts(), color = ['blue', 'red'])
+	plt.bar(['Does have heart disease', 'Does not have heart disease'], heart['target'].value_counts(), color = ['red', 'blue'])
 	plt.ylabel('Count')
 	plt.show()
 
 	# Use dummy columns for the categorical features
-	heart = pd.get_dummies(heart, columns = ['sex', 'cp', 'fbs', 'dm', 'famhist', 'exang'])
-	columns_to_scale = ['age', 'trestbps', 'chol', 'cigs', 'years', 'thalrest', 'trestbpd']
+	# Also use the 
+	# heart = pd.get_dummies(heart, columns = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'ca', 'thal'])
+	heart = pd.get_dummies(heart, columns = ['sex', 'cp', 'fbs'])
 	standardScaler = StandardScaler()
+	# columns_to_scale = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
+	columns_to_scale = ['age', 'trestbps', 'chol']
 	heart[columns_to_scale] = standardScaler.fit_transform(heart[columns_to_scale])
+
 
 	y = heart['target']
 	X = heart.drop(['target'], axis = 1)
@@ -74,6 +93,7 @@ def train_model():
 	plt.ylabel('Scores')
 	plt.title('Decision Tree Classifier scores for different number of maximum features')
 	plt.show()
+
 
 	# Test the KNN classifier
 	# knn_classifier_test = KNeighborsClassifier(n_neighbors = 8)
