ef9c0cd0152be8043f6d7aff50d1194fbc140951
CMorar143 <cian@morar.org>
2019-12-03 01:46:38 +0000

    created a doctor model

--- DjangoProj/DjangoProj/boards/models.py
+++ DjangoProj/DjangoProj/boards/models.py
@@ -6,6 +6,11 @@ class Patient(models.Model):
 	patient_name = models.CharField(max_length=50)
 	DOB = models.DateTimeField()
 	age = models.PositiveIntegerField()
-	address = models.TextField()
+	address = models.TextField(max_length=400)
 	occupation = models.CharField(max_length=30)
 	marital_status = models.PositiveIntegerField()
+
+class Doctor(models.Model):
+	name = models.CharField(max_length=30)
+	username = models.CharField(max_length=20, unique=True)
+	pin = models.PositiveIntegerField(unique=True)
