de909e3ffa517da7a04b228497c47e34d7daeca1
CMorar143 <cian@morar.org>
2019-12-03 01:33:35 +0000

    created a patient model

--- DjangoProj/DjangoProj/boards/models.py
+++ DjangoProj/DjangoProj/boards/models.py
@@ -2,3 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Patient(models.Model):
+	patient_name = models.CharField(max_length=50)
+	DOB = models.DateTimeField()
+	age = models.PositiveIntegerField()
+	address = models.TextField()
+	occupation = models.CharField(max_length=30)
+	marital_status = models.PositiveIntegerField()
