b36ba7b707236dbe479f921d0bbf7d7025aea117
CMorar143 <cian@morar.org>
2019-12-06 02:27:30 +0000

    testing whether the URL matches that in the urls.py file

--- DjangoProj/DjangoProj/boards/tests.py
+++ DjangoProj/DjangoProj/boards/tests.py
@@ -1,5 +1,6 @@
-from django.urls import reverse
+from django.urls import reverse, resolve
 from django.test import TestCase
+from .views import login
 
 # Create your tests here.
 class LoginTests(TestCase):
@@ -7,3 +8,7 @@ class LoginTests(TestCase):
 		url = reverse('login')
 		response = self.client.get(url)
 		self.assertEquals(response.status_code, 200)
+
+	def test_login_url_resolves_login_view(self):
+		view = resolve('/')
+		self.assertEquals(view.func, login)
