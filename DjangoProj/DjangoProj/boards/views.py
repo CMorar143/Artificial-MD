a9834d2af922bd5411c0ea06aaf1945acb644031
CMorar143 <cian@morar.org>
2019-12-06 00:36:52 +0000

    fixed html error

--- DjangoProj/DjangoProj/boards/views.py
+++ DjangoProj/DjangoProj/boards/views.py
@@ -5,10 +5,10 @@ from .models import Patient
 # Create your views here.
 def home(request):
 	patients = Patient.objects.all()
-	patient_names = list()
+	# patient_names = list()
 
-	for patient in patients:
-		patient_names.append(patient.patient_name)
+	# for patient in patients:
+	# 	patient_names.append(patient.patient_name)
 
-	response_html = 'Patient Names are: <br>' + '<br>'.join(patient_names)
-	return HttpResponse(response_html)
+	# response_html = 'Patient Names are: <br>' + '<br>'.join(patient_names)
+	return render(request, 'home.html', {'patients': patients})
