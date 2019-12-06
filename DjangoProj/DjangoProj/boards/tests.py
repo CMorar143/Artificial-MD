e389c2edfb220ed709ad5a56c37fccb6f48c5888
CMorar143 <cian@morar.org>
2019-12-06 02:17:46 +0000

    fixed import statement

--- DjangoProj/DjangoProj/boards/tests.py
+++ DjangoProj/DjangoProj/boards/tests.py
@@ -1,9 +1,9 @@
+from django.urls import reverse
 from django.test import TestCase
-from django.core.urlresolvers import reverse
 
 # Create your tests here.
 class LoginTests(TestCase):
 	def test_login_view_status_code(self):
-		url = reverse('home')
+		url = reverse('login')
 		response = self.client.get(url)
 		self.assertEquals(response.status_code, 200)
