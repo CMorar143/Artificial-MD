661d6e3867686041112b23eef4072b91ccc19553
CMorar143 <cian@morar.org>
2020-01-29 00:04:07 +0000

    added naive bayesto knn

--- Heart_Classifier.py
+++ Heart_Classifier.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
+from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
