5517788f4823e68fe8b89267d38a8f04d4cda660
CMorar143 <cian@morar.org>
2020-02-08 17:47:40 +0000

    added visit table

--- DjangoProj/DjangoProj/boards/models.py
+++ DjangoProj/DjangoProj/boards/models.py
@@ -12,7 +12,41 @@ class Patient(models.Model):
 	marital_status = models.PositiveIntegerField()
 
 	def __str__(self):
-		return self.patient_name
+		fields = (
+			self.patient_name, self.age, self.address,
+			self.occupation, self.marital_status
+		)
+		return str(fields)
+
+
+
+class Family(models.Model):
+	family_name = models.CharField(max_length=30)
+	family_hist = models.CharField(max_length=100, null=True)
+
+	fields = (
+			self.family_name, self.family_hist
+		)
+		return str(fields)
+
+
+
+class Visit(models.Model):
+	doctor = models.ForeignKey('Doctor', on_delete=models.CASCADE)
+	patient = models.ForeignKey('Patient', on_delete=models.CASCADE)
+	date = models.DateTimeField(auto_now_add=True)
+	patient_symptoms = models.CharField(max_length=300, null=True)
+	doctor_notes = models.CharField(max_length=100, null=True)
+	outcome = models.CharField(max_length=50)
+
+	def __str__(self):
+		fields = (
+			self.date, self.patient_symptoms, 
+			self.doctor_notes, self.outcome
+		)
+		return str(fields)
+
+
 
 class Doctor(models.Model):
 	name = models.CharField(max_length=30)
@@ -20,7 +54,12 @@ class Doctor(models.Model):
 	pin = models.PositiveIntegerField(unique=True)
 
 	def __str__(self):
-		return self.username
+		fields = (
+			self.name, self.username, self.pin
+		)
+		return str(fields)
+
+
 
 class Examination(models.Model):
 	age = models.PositiveIntegerField(default=0)
