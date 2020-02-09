ba4f8dddfbde3e454ceb1d0ebaea32f68b31dddb
CMorar143 <cian@morar.org>
2020-02-09 20:55:23 +0000

    rebuilt database with new tables

--- DjangoProj/DjangoProj/boards/admin.py
+++ DjangoProj/DjangoProj/boards/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Patient, Doctor, Examination
+from .models import Patient, Family, Visit, Ailment, Reminders, Allergy, Medical_history, Investigation, Medication, Doctor, Examination
 
 admin.site.register(Patient)
+admin.site.register(Family)
+admin.site.register(Visit)
+admin.site.register(Ailment)
+admin.site.register(Reminders)
+admin.site.register(Allergy)
+admin.site.register(Medical_history)
+admin.site.register(Investigation)
+admin.site.register(Medication)
 admin.site.register(Doctor)
 admin.site.register(Examination)
 
