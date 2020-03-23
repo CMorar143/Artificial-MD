b3db96f1e8435d06c73d7b3edbd14f99b3aef3e7
CMorar143 <cian@morar.org>
2020-03-23 21:56:25 +0000

    trying to calculate entropy for attributes

--- id3.py
+++ id3.py
@@ -9,31 +9,42 @@ def load_dataframe():
 	print(heart.head())
 	return heart
 
-def get_unique(feature):
-	return feature.unique()
-
-def get_df_entropy(heart):
-	# Get target class
-	target = heart.keys()[-1]
+def get_target_entropy(heart):
 	entropy = 0
 
-	# Values are they have heart disease or they don't (1 or 0 respectively)
-	values = get_unique(heart[target])
+	# Possible values are they have heart disease or they don't (1 or 0 respectively)
+	values = heart['target'].unique()
 
+	# Calculate entropy
 	for value in values:
-		val_split = heart[target].value_counts()[value]/len(heart[target])
-		print(val_split)
+		val_split = heart['target'].value_counts()[value]/len(heart['target'])
 		entropy = entropy + -val_split*np.log2(val_split)
-		print(entropy)
+
 	return entropy
 
+def get_feature_entropy(heart, feature):
+	feature_entropy = 0
 
+	# Values are they have heart disease or they don't (1 or 0 respectively)
+	values = heart['target'].unique()
+	feature_vals = heart[feature].unique()
+
+	for value in feature_vals:
+		for val in values:
+			val_split = heart['target'].value_counts()[val]/len(heart['target'])
+			entropy = entropy + -val_split*np.log2(val_split)
+
+	return entropy
+
+# Load dataset
 heart = load_dataframe()
 
-print(heart)
+# Get entropy of target feature
+target_entropy = get_target_entropy(heart)
 
-entropy = get_df_entropy(heart)
-print(entropy)
-
-
+feature = 'sex'
+print(heart[feature].value_counts())
+num = len(heart[feature][heart[feature]==0.0][heart['target'] ==1.0])
+print(num)
+print(heart[feature][heart[feature]==0.0])
 # Next we find the entropy of every other feature
